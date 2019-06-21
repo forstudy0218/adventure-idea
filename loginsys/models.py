a7414ccf4f71978c4468f4994a876ef3acbf03e5
Frank Wong <forstudy0218@gmail.com>
2019-06-21 11:42:57 +0000

    database test01

--- loginsys/models.py
+++ loginsys/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class AdventureRecords(models.Model):
+    devilzone = models.IntegerField()
+    village = models.IntegerField()
+    cave = models.IntegerField()
+    capitalroad = models.IntegerField()
+    desert = models.IntegerField()
+    palace = models.IntegerField()
+    mazetower = models.IntegerField()
+    storybook = models.IntegerField()
+    bossroom = models.IntegerField()
+    blacktower = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.blacktower}{self.bossroom}{self.storybook}{self.mazetower}{self.palace}{self.desert}{self.capitalroad}{self.cave}{self.village}{self.devilzone}"
--- loginsys/views.py
+++ loginsys/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import AdventureRecords
 
 # Create your views here.
 def index(request):
+    context = {
+        "adRec": AdventureRecords.objects.all()
+    }
     # no html
-    return HttpResponse("Hello, test.")
+    # return HttpResponse("Hello, test.")
+    return render(request, "loginsys/home.html", context)
